71cf2735d133f681db70510c48af94c11e90bc65
Chris Nunes <chrisnunes57@gmail.com>
2018-06-23 14:44:44 -0500

    Add files via upload

--- src/components/SliderImage/SliderContainer.js
+++ src/components/SliderImage/SliderContainer.js
@@ -1,42 +1,26 @@
 import React, {Component} from 'react';
-import SliderImage from "./SliderImage";
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class SliderContainer extends Component {
 
-    constructor(props) {
-        super(props);
-        let backgrounds = [];
-        [1, 2, 3, 4].forEach((item) => {
-                backgrounds.push("HomeImages/livingroom" + item + "min.jpg");
-            }
-        );
-        let newBG = backgrounds.map(imgUrl => <SliderImage imgURL={imgUrl} content={this.props.content}/>);
-        this.state = {
-            bgPos: 0,
-            backgrounds: newBG
-        };
-    }
-
-    componentDidMount() {
-        this.timer = setInterval(this.changeBackground, 3000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
-
-
-    changeBackground = () => {
-        if (this.state.bgPos + 1 >= this.state.backgrounds.length) {
-            this.setState({bgPos: 0});
-        } else {
-            let currentState = this.state.bgPos;
-            this.setState({bgPos: currentState + 1});
-        }
-    };
-
     render() {
-        return this.state.backgrounds[this.state.bgPos];
+        return(
+            <Carousel style={styles} showStatus={false} autoPlay={true} showArrows={false} interval={10000} infiniteLoop={true} showIndicators={false} showThumbs={false} transitionTime={1200} stopOnHover={false}>
+                <div>
+                    <img src="HomeImages/livingroom1min.jpg" />
+                </div>
+                <div>
+                    <img src="HomeImages/livingroom2min.jpg" />
+                </div>
+                <div>
+                    <img src="HomeImages/livingroom3min.jpg" />
+                </div>
+                <div>
+                    <img src="HomeImages/livingroom4min.jpg" />
+                </div>
+            </Carousel>
+        )
     }
 }
 
